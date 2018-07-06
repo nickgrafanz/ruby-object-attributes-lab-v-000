eeea2e9006dec365b103ae6ab7a59c8285a312bf
Nick Graffagna <n.graffagna@gmail.com>
2018-07-06 21:25:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog
-
-  def name(name)
-    @name = name
-  end
+  
   def name=(new_name)
     @name = new_name
+  end
+
+  def name
+    @name = name
   end
   def breed(breed)
     @breed = breed
